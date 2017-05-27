1bd400e114db1c3f82520345db6a6c0539ec73a2
Czerwin <r.j.czerwinski@gmail.com>
2017-05-27 16:03:27 +0200

    First function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function getTriangleArea(a, h) {
+    if ( (a > 0) && (h > 0) ) {
+      console.log(getTriangleArea);
+    }  else {
+      console.log('Nieprawidłowe dane');
+    }
+      return(a*h/2)
+}
+console.log(getTriangleArea(10,6) );
+
+var triangle1Area = getTriangleArea(6, 9)
+    console.log(triangle1Area);
+  triangle2Area = getTriangleArea(11, 25)
+    console.log(triangle2Area);
+  triangle3Area = getTriangleArea(18, 5)
+    console.log(triangle3Area);
