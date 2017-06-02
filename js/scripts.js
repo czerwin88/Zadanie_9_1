af07a4458742359482cdad7a328037f91976715c
Czerwin <r.j.czerwinski@gmail.com>
2017-06-02 17:43:21 +0200

    Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,5 @@ function getTriangleArea(a, h) {
 console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(6, 9);
-  triangle2Area = getTriangleArea(11, 25);
-  triangle3Area = getTriangleArea(18, 5);
+var triangle2Area = getTriangleArea(11, 25);
+var triangle3Area = getTriangleArea(18, 5);
