5ca077a4171b30709ce8a90de57c5a7ad9ce6acf
Czerwin <r.j.czerwinski@gmail.com>
2017-06-02 17:41:04 +0200

    Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 function getTriangleArea(a, h) {
-    if ( (a > 0) && (h > 0) ) {
+    console.log((a > 0) && (h > 0) ? a * h / 2 : 'Nieprawidłowe dane')
+
+    /*if ( (a > 0) && (h > 0) ) {
       console.log(getTriangleArea);
     }  else {
       console.log('Nieprawidłowe dane');
-    }
-      return(a*h/2);
-}
-console.log(getTriangleArea(10,6) );
+    }*/
 
-var triangle1Area = getTriangleArea(6, 9)
-    console.log(triangle1Area);
-  triangle2Area = getTriangleArea(11, 25)
-    console.log(triangle2Area);
-  triangle3Area = getTriangleArea(18, 5)
-    console.log(triangle3Area);
+    return;
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(6, 9);
+  triangle2Area = getTriangleArea(11, 25);
+  triangle3Area = getTriangleArea(18, 5);
